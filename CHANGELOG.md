48adea7e2acd515eb26c2597c5ff8db608f291d0
emersonsiega <emersonsiega@hotmail.com>
2021-07-13 14:49:24 -0300

    chore: Update changelog and remove comments

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,8 +3,10 @@
 All notable changes to the "flutter-monorepo-utils" extension will be documented in this file.
 
 ## [0.0.2] - 13/07/2021
-- Adds a validation to the check the selected folder before execute the test commands
+
+- Adds a validation to check the selected folder before execute the test commands
 - Improve Test runner documentation
+- Fix Test runner execution on Windows
 
 ## [0.0.1] - 13/07/2021
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,3 @@
-// ------------------------------------------------------------------------------------
-/**
- * TODO:
- *  [X] - Run tests (context menu)
- *  [] - Improve context menu location and package name
- *  [] - Run ALL tests (context menu)
- *  [] - Run get/clean/build_runner (context menu)
- *  [] - Run tests when project changes
- *  [] - Create test file in same hierarchy
- *  [] - Dream: Load and watch all tests (grouped by package) in the testing tab
- */
-// ------------------------------------------------------------------------------------
-
 import * as vscode from 'vscode';
 import { runTests } from './utilities/test_runner';
 import { packageIdentifier } from './constants';
